32969aee886d2ff522bf132e0a07888af009d152
ramithhabeeb <habeebramithkumar04@gmail.com>
2025-03-12 11:19:03 +0530

    Made logo and basic template for signup

---
+++ signup-page/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header className='header'>
+      <img src="/public/Fleet Logo.png" alt="Fleet Logo" />
+      <h1 >FLEET</h1>
+    </header>
+    <div className='logo'>
+      <img src="/public/Fleet Logo.png" alt="Fleet Logo" />
+      <h1>FLEET</h1>
+      <p>DRIVE YOUR JOURNEY ANYTIME, ANYWHERE</p>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
